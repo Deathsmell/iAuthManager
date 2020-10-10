913a954260f059f9853c8148e3a1007ccbd759d2
Alexander Mitskevich <johndeathsmell@gmail.com>
2020-10-10 23:39:26 +0300

    fixed remove token then refresh page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const {Router} = require('express')
 
 const app = express()
 const router = Router()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const API = config.get('api') || 'api'
 
 app.use(express.static("public"))
